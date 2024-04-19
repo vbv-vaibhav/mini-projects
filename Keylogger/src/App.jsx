2c6d6d98ef69bf1353c80ee5c4383d9d399d6c11
Vaibhav Bansal <vbvbansal@gmail.com>
2024-04-19 12:08:44 +0530

    logging button is not dynamic

--- Keylogger/src/App.jsx
+++ Keylogger/src/App.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [logging, setLogging] = useState(true);
+  const [loggedValue, setLoggedValue] = useState("");
+
   return (
     <>
       <div className="grid grid-cols-2">
@@ -9,20 +12,18 @@ const App = () => {
             <div className="text-5xl font-medium p-4">Visual Key Logger</div>
           </div>
           <div className="text-center">
-            <button className="p-4 m-2 bg-slate-400 rounded-lg font-medium">
-              Start Logging
-            </button>
-          </div>
-          <div className="text-center">
-            <button className="p-4 m-2 bg-slate-400 rounded-lg font-medium">
-              Stop Logging
+            <button
+              onClick={() => setLogging(!logging)}
+              className="p-4 m-2 bg-slate-400 rounded-lg font-medium"
+            >
+              {logging ? "Stop Logging" : "Start Logging"}
             </button>
           </div>
         </div>
         <div className="bg-red-100 h-screen flex flex-col justify-center">
           <div className="flex justify-center">
             <div className="bg-slate-300 w-1/2 text-center text-4xl p-5">
-              New div
+              {loggedValue}
             </div>
           </div>
         </div>
