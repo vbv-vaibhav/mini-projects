01f726fdf455a830acbf914e716b2781cb1d061e
Vaibhav Bansal <vbvbansal@gmail.com>
2024-04-22 11:31:01 +0530

    final prototype

--- Keylogger/src/App.jsx
+++ Keylogger/src/App.jsx
@@ -33,25 +33,36 @@ const App = () => {
   return (
     <>
       <div className="grid grid-cols-2">
-        <div className="bg-green-100 h-screen flex flex-col justify-center">
-          <div className="text-center m-7">
-            <div className="text-5xl font-medium p-4">Visual Key Logger</div>
+        <div className="bg-slate-100 h-screen flex flex-col justify-center">
+          <div className="text-center m-10">
+            <div className="text-5xl font-medium m-4 hover:scale-105 duration-300">
+              Visual Key Logger
+            </div>
           </div>
           <div className="text-center">
             <button
               onClick={logging ? remLogFunc : logFunc}
-              className="p-4 m-2 bg-slate-400 rounded-lg font-medium"
+              className="p-4 bg-blue-500 rounded-lg hover:scale-105 duration-300 hover:shadow-md hover:shadow-blue-950"
             >
               {logging ? "Stop Logging" : "Start Logging"}
             </button>
           </div>
         </div>
-        <div className="bg-red-100 h-screen flex flex-col justify-center">
+        <div className="h-screen flex flex-col justify-center">
           <div className="flex justify-center flex-col items-center">
-            <div className="bg-slate-300 text-center text-4xl p-5 my-8 h-[70px] w-[70px] rounded-xl">
-              {loggedValue[loggedValue.length - 1]}
+            <div className="">
+              <div className="font-medium text-2xl p-2 text-center">
+                Keystrokes
+              </div>
+              <div className="bg-slate-300 text-center text-4xl p-5 mb-8 h-[70px] w-[200px] rounded-xl hover:shadow-md hover:shadow-slate-500 duration-300">
+                {loggedValue[loggedValue.length - 1]}
+              </div>
             </div>
-            <div className="bg-slate-300 w-1/2 text-center text-4xl p-5 min-h-[100px] my-4">
+
+            <div className="font-medium text-2xl p-2 text-center">
+              Resulting String
+            </div>
+            <div className="bg-slate-300 w-1/2 text-center text-2xl p-5 min-h-[75px] min-w-[600px] rounded-xl hover:shadow-md hover:shadow-slate-500 duration-300">
               {loggedValue.join(", ")}
             </div>
           </div>
@@ -62,8 +73,3 @@ const App = () => {
 };
 
 export default App;
-
-//Styling
-//Implement single key press style
-//Add a full string element
-//Add a copy button
